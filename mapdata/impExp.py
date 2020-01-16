d35696c05d3ed941935d85f1a57f87ab94f64174
orsu <theo181297@gmail.com>
2020-01-16 23:16:49 +0100

    made a python script automatically converting gltf to stl

---
+++ mapdata/impExp.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import bpy
+import sys
+import os
+
+argv = sys.argv
+
+argv = argv[argv.index("--") + 1:]  # get all args after "--"
+
+print(argv)  # --> ['example', 'args', '123']
+
+if len(argv[0]) > 0:
+    inputfile = argv[0]
+else:
+    print("Error. No inputed file.")
+
+if len(argv) >= 2:
+    outputfile = argv[1]
+else:
+    outputfile = argv[0] + ".stl"
+
+bpy.ops.import_scene.gltf(filepath=inputfile)
+bpy.ops.export_mesh.stl(filepath=outputfile)
