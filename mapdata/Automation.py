dcab08637cc57e5f4c0299c16bfde174fa98be21
orsu <theo181297@gmail.com>
2020-01-17 04:10:03 +0100

    added qt selector
    THO : it's not really good. We need to find a better way to pick the work directory

--- mapdata/Automation.py
+++ mapdata/Automation.py
@@ -12,7 +12,11 @@
 """
 
 from PyQt5.QtCore import (QCoreApplication,
-                        QFileInfo)
+                        QFileInfo,
+                        QSettings)
+
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit
+
 from qgis.core import (QgsProcessing,
                        QgsFeatureSink,
                        QgsProcessingException,
@@ -21,11 +25,46 @@ from qgis.core import (QgsProcessing,
                        QgsProcessingParameterFeatureSink,
                        QgsRasterLayer,
                        QgsProject,
-                       QgsVectorLayer)
+                       QgsVectorLayer,
+                       QgsCoordinateReferenceSystem)
 import processing
 import sys, os
 
+class App(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 input dialogs - pythonspot.com'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+        self.initUI()
+    
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        
+        self.getInteger()
+        self.getText()
+        
+        self.show()
+        
+    def getInteger(self):
+        i, okPressed = QInputDialog.getInt(self, "CRS ID","Input a valid CRS ID:", 28, 0, 999999999, 1)
+        if okPressed:
+            CRS_ID = i
+            print(i)
+
+    def getText(self):
+        text, okPressed = QInputDialog.getText(self, "WORKDIR","Input your workfolder :", QLineEdit.Normal, "")
+        if okPressed and text != '':
+            WORKDIR = text
+            print(text)
+            
+
 WORKDIR = "C:/Users/theo1/Documents/GitHub/Hackathon2020/mapdata/"
+CRS_ID = 2154 # CRS id we'll apply to all layers. This is lambert
 
 def importFirstTif():
     # import the first Tif found in this folder
@@ -44,6 +83,8 @@ def importFirstTif():
         path = fileInfo.filePath()
         basename = fileInfo.baseName()
         layer = QgsRasterLayer(path, basename)
+        layer.setCrs(QgsCoordinateReferenceSystem(CRS_ID, QgsCoordinateReferenceSystem.EpsgCrsId))
+        
         QgsProject.instance().addMapLayer(layer)
         
         if layer.isValid() is True:
@@ -75,6 +116,7 @@ def importAllShapes():
             # vlayer = QgsVectorLayer(data_source, layer_name, provider_name)
 
             vlayer = QgsVectorLayer(path_to_ports_layer, shapeF, "ogr")
+            vlayer.setCrs(QgsCoordinateReferenceSystem(CRS_ID, QgsCoordinateReferenceSystem.EpsgCrsId))
             QgsProject.instance().addMapLayer(vlayer)
             if not vlayer.isValid():
                 print("Layer failed to load!")
@@ -95,6 +137,7 @@ def importFirstDXF():
     try:
 
         layer = QgsVectorLayer(WORKDIR + file + "|layername=entities|geometrytype=LineString", file, "ogr")
+        layer.setCrs(QgsCoordinateReferenceSystem(CRS_ID, QgsCoordinateReferenceSystem.EpsgCrsId))
         QgsProject.instance().addMapLayer(layer)
         
         if layer.isValid() is True:
@@ -105,6 +148,10 @@ def importFirstDXF():
     except Error as e:
         print("Error loading raster file")
         print(e)
+
+app = QApplication(sys.argv)
+ex = App()
+sys.exit(app.exec_())
 
 importFirstTif()
 importFirstDXF()
